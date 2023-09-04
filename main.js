2b52cf31c1cbe3ed3eb08782a809489731837161
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2023-09-04 10:47:07 -0300

    04/09/2023

---
+++ main.js
@@ -0,0 +1,48 @@
+function tocaSomPom (){
+    document.queryselector('#som_tecla_pom').play();
+}
+
+document.queryselector('.tecla_pom').onclick=tocaSomPom;
+
+function tocaSomClap (){
+    document.queryselector('#som_tecla_Clap').play();
+}
+
+document.queryselector('.tecla_Clap').onclick = tocaSomClap;
+
+function tocaSomTim (){
+    document.queryselector('#som_tecla_Tim').play();
+}
+
+document.queryselector('.tecla_Tim').onclick=tocaSomTim;
+
+function tocaSomPuff (){
+    document.queryselector('#som_tecla_puff').play();
+}
+
+document.queryselector('.tecla_puff').onclick=tocaSomPuff;
+
+function tocaSomSplash (){
+    document.queryselector('#som_tecla_splash').play();
+}
+
+document.queryselector('.tecla_splash').onclick=tocaSomSplash;
+
+function tocaSomPom (){
+    document.queryselector('#som_tecla_pom').play();
+}
+
+document.queryselector('.tecla_pom').onclick=tocaSomPom;
+
+function tocaSomToim (){
+    document.queryselector('#som_tecla_toim').play();
+}
+
+document.queryselector('.tecla_toim').onclick=tocaSomToim;
+
+function tocaSomPom (){
+    document.queryselector('#som_tecla_psh').play();
+}
+
+document.queryselector('.tecla_psh').onclick=tocaSomPsh;
+
